4db1007681cf2c4e1413d5b1f6be53ecc2dedccf
Harsh Lakhani <20it061@charusat.edu.in>
2021-08-12 18:39:22 +0530

    ChefAndOperator

---
+++ ChefAndOperator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ChefAndOperator {
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        for (int i = 0; i < n ;i++) {
+            int A = s.nextInt();
+            int B = s.nextInt();
+
+            if(A < B){
+                System.out.println("<");
+            } else if (A > B){
+                System.out.println(">");
+            }  else {
+                System.out.println("=");
+            }
+
+        }
+    }
+}
