91f36cce8434c6c803b681165b8bf4382cfa3d42
Mit076er <20it076@charusat.edu.in>
2021-08-16 22:11:51 +0530

    IncreORDecre

---
+++ IncreORDecre.java
@@ -0,0 +1,20 @@
+package com.mycompany.incordec;
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n % 4 == 0){
+		    System.out.println(n+1);
+		}else System.out.println(n-1);
+	}
+}
