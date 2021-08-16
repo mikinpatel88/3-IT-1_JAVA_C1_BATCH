91b32f5dfa0d4dff72d9b3e2fae2c26da40f0fc7
17mahir <88772847+17mahir@users.noreply.github.com>
2021-08-16 11:46:39 +0530

    Add files via upload

---
+++ ChefAndOperators.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+ class ChefAndOperators {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n= s.nextInt();
+
+        for(int i=0;i<n;i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            if (a > b)
+                {System.out.println(">");
+                }
+            else if (a < b)
+                {System.out.println("<");
+                }
+            else
+                {System.out.println("=");
+                }
+        }
+
+
+
+    }
+}
