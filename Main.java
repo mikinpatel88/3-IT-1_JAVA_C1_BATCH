32f923255cb9bc382fb446801db82b45cbaf6070
Akshatparikh19 <20it078@charusat.edu.in>
2021-08-17 21:32:23 +0530

    Fisrt commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%4==0)
+        {
+            System.out.println(++n);
+        }
+        else
+        {
+            System.out.println(--n);
+        }
+    }
+}
