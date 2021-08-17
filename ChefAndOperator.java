1316046603130ae190fee0ae2a02a456ace5b6f7
20it079 <88765812+20it079@users.noreply.github.com>
2021-08-17 12:24:45 +0530

    Add files via upload

---
+++ ChefAndOperator.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+public class ChefAndOperator {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of test cases");
+        int t = sc.nextInt();
+        for (int i = 0; i < t ;i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if(A < B){
+                System.out.println("<");
+            } else if (A > B){
+                System.out.println(">");
+            }  else {
+                System.out.println("=");
+            }
+
+        }
+    }
+}
