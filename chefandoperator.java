b9cd82dc0758415cefbfdf20d07e5c0902b3c171
107Neel <20IT071@charusat.edu.in>
2021-08-18 22:51:08 +0530

    first commit

--- chefandoperator.java
+++ chefandoperator.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 class chefandoperator
-{cd
+{cdgit
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc = new Scanner(System.in);
