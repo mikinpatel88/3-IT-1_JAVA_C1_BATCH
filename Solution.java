2354a83b69f131eaeb1d2d398e15dbf0689e9aa2
Mit076er <88765741+Mit076er@users.noreply.github.com>
2021-08-18 13:37:29 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,15 @@
+package com.mycompany.javaendoffile;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution{
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);  
+        int i = 0;
+        while (sc.hasNextLine()) {
+            System.out.println(++i + " " + sc.nextLine());
+        }
+    }
+}
