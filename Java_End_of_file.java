bffcb79f741c6766e7ac36a1f317c2b06739054a
VIRAJ  KORADIA <88263251+viraj0076@users.noreply.github.com>
2021-08-19 17:44:00 +0530

    Add files via upload

---
+++ Java_End_of_file.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class Java_End_of_file {
+    public static void main(String[] args) {
+
+
+            Scanner scan = new Scanner(System.in);
+            int i = 1;
+            while (scan.hasNext()) {
+                System.out.println(i + " " + scan.nextLine());
+                i++;
+            }
+            scan.close();
+        }
+    }
+
