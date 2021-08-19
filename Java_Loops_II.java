c3bdbafba0bbf06d88329abde826d785e51e7904
VIRAJ  KORADIA <88263251+viraj0076@users.noreply.github.com>
2021-08-19 17:16:13 +0530

    Add files via upload

---
+++ Java_Loops_II.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Java_Loops_II {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for (int j = 0; j < n; j++) {
+                a += b * (int) Math.pow(2, j);
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+    }
+
