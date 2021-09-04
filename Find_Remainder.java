1cb8b8448bc459b5b014f36e20aef0eb276abf58
Parth Mangukiya <88765995+20IT066@users.noreply.github.com>
2021-09-04 11:03:23 +0530

    Add files via upload

---
+++ Find_Remainder.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+
+public class Find_Remainder {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = a%b;
+            System.out.println(c);
+        }
+    }
+}
