c0e3bc81e52d133954d4093319b3e29f8ca777b8
NISHANT MIYANI <88224248+Nishant1782@users.noreply.github.com>
2021-09-04 15:38:18 +0530

    Create reverse_the_number.java

---
+++ reverse_the_number.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+
+			System.out.println(solve(N));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int N) {
+		return Integer.parseInt(new StringBuilder(String.valueOf(N)).reverse().toString());
+	}
+}
