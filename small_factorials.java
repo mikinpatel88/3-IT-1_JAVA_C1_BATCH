ce35837a4872948713f682d8c65da2d6e90b7b5b
NISHANT MIYANI <88224248+Nishant1782@users.noreply.github.com>
2021-09-04 15:30:09 +0530

    Create small_factorials.java

---
+++ small_factorials.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+		int numOfCases = sc.nextInt();
+        while (numOfCases-- > 0) {
+            int n = sc.nextInt();
+            BigInteger factor = new BigInteger("1");
+            for (int i = 2 ; i <= n; i++) {
+                factor = factor.multiply(BigInteger.valueOf(i));
+
+            }
+            System.out.println(factor);
+        }
+        sc.close();
+	}
+}
