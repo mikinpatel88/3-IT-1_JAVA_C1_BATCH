2e66800d33e0795b48297aacad3ff14e933579b2
Mit076er <88765741+Mit076er@users.noreply.github.com>
2021-09-12 09:26:06 +0530

    Add files via upload

---
+++ FindSquareroot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    
+	    for(int i=0; i<t; i++){
+	        int n = sc.nextInt();
+	        System.out.println((int)Math.sqrt(n));
+	    }
+	}
+}
