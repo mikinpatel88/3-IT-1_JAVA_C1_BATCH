7953783bb0fcc4377cc7f3888530355eb5fec120
HarshParmar30 <89096443+HarshParmar30@users.noreply.github.com>
2021-09-12 19:31:53 +0530

    Add files via upload

---
+++ ChefAndRemissions.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ChefAndRemissions
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc= new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int i=1; i<=T; i++)
+        {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if(A>B)
+                System.out.println(A+" "+(A+B));
+            else
+                System.out.println(B+" "+(A+B));
+        }
+    }
+}
