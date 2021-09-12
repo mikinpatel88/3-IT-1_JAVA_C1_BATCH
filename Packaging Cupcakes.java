26d50f328be30119a6b4e362ceba0e24795e22f7
prathammehta16 <72267555+prathammehta16@users.noreply.github.com>
2021-09-12 12:15:11 +0530

    Add files via upload

---
+++ Packaging Cupcakes.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner(System.in);
+
+		// Get the total nos
+		int t =input.nextInt();
+
+		for(int i =0;i<t;i++){
+		    int a  = input.nextInt();
+		    System.out.println((a/2)+1);
+		}
+
+	}
+}
