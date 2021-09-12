862851d8c5e1bda338a2f64019e3b935134e14fc
HarshParmar30 <89096443+HarshParmar30@users.noreply.github.com>
2021-09-12 19:46:21 +0530

    Add files via upload

---
+++ CielAndReceipt.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CielAndReceipt
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc= new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int i=1; i<=T; i++)
+        {
+            int count=0;
+            double p = sc.nextDouble();
+            for(int j=11; j>=0; j--)
+            {
+                double menu = Math.pow(2, j);
+                while (p >= menu)
+                {
+                    p = p - menu;
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
