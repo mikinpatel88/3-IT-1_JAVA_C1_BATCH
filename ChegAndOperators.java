6443d2bed2775fc94ca62c627662c77862c9ef02
HarshParmar30 <89096443+HarshParmar30@users.noreply.github.com>
2021-09-12 19:27:35 +0530

    Add files via upload

---
+++ ChegAndOperators.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ChefAndOperators
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        for (int i = 0; i < n ;i++) {
+            int A = s.nextInt();
+            int B = s.nextInt();
+
+            if(A < B){
+                System.out.println("<");
+            } else if (A > B){
+                System.out.println(">");
+            }  else {
+                System.out.println("=");
+            }
+
+        }
+    }
+}
