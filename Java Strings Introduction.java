bec5ba959c4611a744d7b4e10257f4b9af4b52e2
prathammehta16 <72267555+prathammehta16@users.noreply.github.com>
2021-09-12 12:56:50 +0530

    Add files via upload

---
+++ Java Strings Introduction.java
@@ -0,0 +1,13 @@
+package com.java;
+import java.io.*;
+import java.util.*;
+public class intro {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+
+    }
+}
