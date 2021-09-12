90f5dc120816a5db221f756b1820af02d1391ad5
prathammehta16 <72267555+prathammehta16@users.noreply.github.com>
2021-09-12 13:01:13 +0530

    Add files via upload

---
+++ Java Strings Reverse.java
@@ -0,0 +1,13 @@
+package com.java;
+import java.io.*;
+import java.util.*;
+
+public class reverse {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        
+
+    }
+}
