1572471ba949194f9625cccac17295ed79c93492
Chintanvck <88276991+Chintanvck@users.noreply.github.com>
2021-09-15 12:28:17 +0530

    Add files via upload

---
+++ SumOfDigit.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class SumOfDigit
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+
+        for(int t=0;t<T;t++)
+        {
+            int num=sc.nextInt();
+            int sum=0;
+            do
+            {
+                int rem=num%10;
+                sum+=rem;
+                num/=10;
+            }while(num>0);
+            System.out.println(sum);
+        }
+    }
+}
