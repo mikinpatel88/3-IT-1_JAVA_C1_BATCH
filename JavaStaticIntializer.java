a8e4865c0bd3956d1b761c280cf4a70bf2f7fe50
107Neel <88573940+107Neel@users.noreply.github.com>
2021-09-20 15:44:54 +0530

    Add files via upload

---
+++ JavaStaticIntializer.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStaticIntializer {
+    private static int B;
+    private static int H;
+    private static boolean flag;
+
+    static
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        B=sc.nextInt();
+        H=sc.nextInt();
+        sc.close();
+        if(B<=0 || H<=0)
+        {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag=false;
+        }
+        else
+        {
+            flag=true;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        if(flag)
+        {
+            int area=B*H;
+            System.out.print(area);
+        }
+    }
+}
