ad56c3a1a9c2f6b95bd09aaf375ff942c44de507
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2021-10-07 12:58:36 +0530

    Add files via upload

---
+++ HelpingChef.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class HelpingChef {
+
+
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        int t;
+        Scanner sc = new Scanner(System.in);
+        t = sc.nextInt();
+        for(int i = 0;i<t;i++)
+        {
+            int n = sc.nextInt();
+            if(n<10)
+            {
+                System.out.println("Thanks for helping Chef!");
+            }
+            else{
+                System.out.println("-1");
+            }
+        }
+    }
+}
