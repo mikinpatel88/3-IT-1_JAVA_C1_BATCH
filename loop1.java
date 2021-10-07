a5b423400f89412cddf01c2abb5c2f33b3cdd1ee
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2021-10-07 12:49:48 +0530

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class loop1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for( int i = 1; i <= 10; i++ ) {
+            System.out.println( N + " x " + i + " = " + N*i);
+        }
+    }
+}
