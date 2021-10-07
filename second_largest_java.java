2c50d28cd249b016f8f9e794152a8647e165a5cf
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2021-10-07 12:45:45 +0530

    Add files via upload

---
+++ second_largest_java.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class second_largest
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if ( (a < b && b < c) || (b < a && c < b ) ){
+                System.out.println(b);
+            }
+            else if ( ( b < a && a < c) || ( a < b  && c < a) ){
+                System.out.println(a);
+            }
+            else System.out.println(c);
+        }
+
+    }
+}
