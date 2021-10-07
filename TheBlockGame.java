3448ac9388ec2fb65502c8b4bb8f66324a9113d5
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2021-10-07 13:04:11 +0530

    Add files via upload

---
+++ TheBlockGame.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class TheBlockGame
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+
+        Scanner sc= new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=0;i<T;i++)
+        {
+            int N=sc.nextInt();
+            int r,rev=0;
+            int temp=N;
+            while(N>0)
+            {
+                r=N%10;
+                rev=(rev*10)+r;
+                N=N/10;
+            }
+            if(temp==rev)
+                System.out.println("wins");
+            else
+                System.out.println("loses");
+        }
+    }
+}
