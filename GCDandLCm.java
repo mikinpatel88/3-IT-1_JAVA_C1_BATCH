ed5b530cf44013f5569a2b8047a2bf2681b4d939
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2021-10-07 13:05:52 +0530

    Add files via upload

---
+++ GCDandLCm.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GCDandLCM
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+            long n1=sc.nextInt();
+            long n2=sc.nextInt();
+            System.out.println(GCD(n1,n2)+" "+LCM(n1,n2));
+        }
+    }
+    static long GCD(long n1,long n2)
+    {
+        if(n1==0)
+            return n2;
+        return GCD(n2%n1 ,n1);
+    }
+
+    static long LCM(long n1,long n2)
+    {
+        return (n1/GCD(n1,n2))*n2;
+    }
+}
