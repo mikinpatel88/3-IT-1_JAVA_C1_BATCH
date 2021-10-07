d38f5309068f466153e7a63daa15ac35e7da0440
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2021-10-07 12:48:24 +0530

    Add files via upload

---
+++ packingCupcakes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class packingCupcakes {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int T = sc.nextInt();
+        int A=0;
+
+        for(int i=0; i<T; i++)
+        {
+            int N = sc.nextInt();
+            A = (N/2+1);
+            System.out.println(A);
+        }
+    }
+}
