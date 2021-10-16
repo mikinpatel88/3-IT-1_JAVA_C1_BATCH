1e430d4b048b88538f80bb2d0686a67aa1ffba35
Jay_1407 <88769189+jaypadmani@users.noreply.github.com>
2021-10-16 15:55:53 +0530

    Add files via upload

---
+++ VaildTriangles.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class VaildTriangles {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for (int tc = 0; tc < T; tc++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+
+            System.out.println(solve(A, B, C) ? "YES" : "NO");
+        }
+
+        sc.close();
+    }
+
+    static boolean solve(int A, int B, int C) {
+        return A + B + C == 180;
+    }
+
+
+}
