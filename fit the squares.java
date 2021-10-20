5a2553e3c332c7356ac193f5607123cc7b46dbb2
NISHANT MIYANI <88224248+Nishant1782@users.noreply.github.com>
2021-10-20 21:46:57 +0530

    Create fit the squares.java

---
+++ fit the squares.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+       int Test= sc.nextInt();
+       while (Test-->0)
+       {
+           long base= sc.nextInt();
+           base=base-2;
+           base=Math.floorDiv(base,2);
+           System.out.println(base*(base+1)/2);
+       }
+	}
+}
+
